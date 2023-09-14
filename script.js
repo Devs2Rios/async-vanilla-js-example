6bc6b4100d953d10ec7446ce7938118654c925c6
eduairet <hola@eduairet.com>
2023-09-13 22:59:14 -0600

    Get countries data

--- script.js
+++ script.js
@@ -4,3 +4,40 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  const endpoint = 'https://restcountries.com/v3.1/name/';
+  // open(Method, Endpoint)
+  request.open('GET', endpoint + country);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name.common}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  data.population / 1_000_000
+                ).toFixed(1)} people</p>
+                <p class="country__row"><span>🗣️</span>${
+                  Object.values(data.languages)[0]
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  Object.values(data.currencies)[0].name
+                }</p>
+            </div>
+        </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+// {name: {…}, tld: Array(1), cca2: 'PT', ccn3: '620', cca3: 'PRT', …}
+getCountryData('mexico');
+getCountryData('usa');
+getCountryData('germany');
